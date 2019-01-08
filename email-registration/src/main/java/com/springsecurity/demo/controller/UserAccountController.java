a2dedc5a8acd3b639f378c27650c5c3d850cf4e2
chand31290 <chand31290@gmail.com>
2019-01-08 09:24:39 +0530

    Initial Commit

---
+++ email-registration/src/main/java/com/springsecurity/demo/controller/UserAccountController.java
@@ -0,0 +1,119 @@
+package com.springsecurity.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springsecurity.demo.model.ConfirmationToken;
+import com.springsecurity.demo.model.User;
+import com.springsecurity.demo.service.EmailSenderService;
+import com.springsecurity.demo.service.repository.ConfirmationTokenRepository;
+import com.springsecurity.demo.service.repository.UserRepository;
+
+@Controller
+public class UserAccountController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ConfirmationTokenRepository confirmationTokenRepository;
+	
+	@Autowired
+	private EmailSenderService emailSenderService;
+
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+	public ModelAndView displayRegistration(ModelAndView modelAndView, User user)
+	{
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("register");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public ModelAndView registerUser(ModelAndView modelAndView, User user)
+	{
+		
+		User existingUser = userRepository.findByEmailIdIgnoreCase(user.getEmailId());
+		if(existingUser != null)
+		{
+			modelAndView.addObject("message","This email already exists!");
+			modelAndView.setViewName("error");
+		}
+		else 
+		{
+			userRepository.save(user);
+			
+			ConfirmationToken confirmationToken = new ConfirmationToken(user);
+			
+			confirmationTokenRepository.save(confirmationToken);
+			
+			SimpleMailMessage mailMessage = new SimpleMailMessage();
+			mailMessage.setTo(user.getEmailId());
+			mailMessage.setSubject("Complete Registration!");
+			mailMessage.setFrom("chand312902@gmail.com");
+			mailMessage.setText("To confirm your account, please click here : "
+			+"http://localhost:8082/confirm-account?token="+confirmationToken.getConfirmationToken());
+			
+			emailSenderService.sendEmail(mailMessage);
+			
+			modelAndView.addObject("emailId", user.getEmailId());
+			
+			modelAndView.setViewName("successfulRegisteration");
+		}
+		
+		return modelAndView;
+	}
+	
+	
+	
+	@RequestMapping(value="/confirm-account", method= {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView confirmUserAccount(ModelAndView modelAndView, @RequestParam("token")String confirmationToken)
+	{
+		ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+		
+		if(token != null)
+		{
+			User user = userRepository.findByEmailIdIgnoreCase(token.getUser().getEmailId());
+			user.setEnabled(true);
+			userRepository.save(user);
+			modelAndView.setViewName("accountVerified");
+		}
+		else
+		{
+			modelAndView.addObject("message","The link is invalid or broken!");
+			modelAndView.setViewName("error");
+		}
+		
+		return modelAndView;
+	}
+
+	public UserRepository getUserRepository() {
+		return userRepository;
+	}
+
+	public void setUserRepository(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public ConfirmationTokenRepository getConfirmationTokenRepository() {
+		return confirmationTokenRepository;
+	}
+
+	public void setConfirmationTokenRepository(ConfirmationTokenRepository confirmationTokenRepository) {
+		this.confirmationTokenRepository = confirmationTokenRepository;
+	}
+
+	public EmailSenderService getEmailSenderService() {
+		return emailSenderService;
+	}
+
+	public void setEmailSenderService(EmailSenderService emailSenderService) {
+		this.emailSenderService = emailSenderService;
+	}
+	
+}
