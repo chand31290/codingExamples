6e21ec8b713b4d6084621c314fbfa76c5955bfdc
Abdulcelil Cercenazi <celil.cercenazi@gmail.com>
2021-04-11 15:55:32 +0300

    Add @EnableAsync to the SpringBoot project
    To allow the @Async annotation to actually work we need to add the @EnableAsync annotation to the Spring Boot main class

--- email-registration/src/main/java/com/springsecurity/demo/RunApplication.java
+++ email-registration/src/main/java/com/springsecurity/demo/RunApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAsync
 public class RunApplication {
 	public static void main(String[] args) {
         SpringApplication.run(RunApplication.class, args);
